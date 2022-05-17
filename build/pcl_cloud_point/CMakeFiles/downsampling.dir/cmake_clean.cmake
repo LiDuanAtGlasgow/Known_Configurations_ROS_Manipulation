c5eda7e60dd92fd316423f6863d60ebc3d0430ea
LiDuanAtGlasgow <6869995@gmail.com>
2022-05-17 09:29:51 +0100

    Update

---
+++ build/pcl_cloud_point/CMakeFiles/downsampling.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/downsampling.dir/src/downsampling.cpp.o"
+  "/home/kentuen/ros_ws/devel/lib/pcl_cloud_point/downsampling.pdb"
+  "/home/kentuen/ros_ws/devel/lib/pcl_cloud_point/downsampling"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/downsampling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/pcl_cloud_point/CMakeFiles/pcl_point_cloud_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcl_point_cloud_gencfg"
+  "/home/kentuen/ros_ws/devel/include/pcl_point_cloud/algorithmParametersConfig.h"
+  "/home/kentuen/ros_ws/devel/share/pcl_point_cloud/docs/algorithmParametersConfig.dox"
+  "/home/kentuen/ros_ws/devel/share/pcl_point_cloud/docs/algorithmParametersConfig-usage.dox"
+  "/home/kentuen/ros_ws/devel/lib/python2.7/dist-packages/pcl_point_cloud/cfg/algorithmParametersConfig.py"
+  "/home/kentuen/ros_ws/devel/share/pcl_point_cloud/docs/algorithmParametersConfig.wikidoc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/pcl_point_cloud_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
